0a2858e3a4a81b13edd4e31476f28336b7f518b0
77web <hishida77web@gmail.com>
2023-10-01 18:36:49 +0900

    symfony composer require --dev phpunit maker profiler

--- config/bundles.php
+++ config/bundles.php
@@ -7,4 +7,6 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
