a365a7f6c27d9c6c3574ee99a245dd67a7582c51
77web <hishida77web@gmail.com>
2023-10-01 20:26:46 +0900

    composer require --dev zenstruck/foundry

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
 ];
